6da78f6ab544bb7d0fa5cd4c7da9a268d8a0e53e
Ksenia <mineeva.94@inbox.ru>
2022-12-18 19:07:42 +0300

    Домашнее задание 4

---
+++ task29/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив
+из 8 элементов и выводит их на экран.*/
+
+void arrayZero (int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        col[i] = new Random().Next(col.Length);
+    }
+} 
+void printArray (int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write($"{col[i]} ");
+    }
+}
+int[] array = new int[8];
+arrayZero(array);
+printArray(array);
