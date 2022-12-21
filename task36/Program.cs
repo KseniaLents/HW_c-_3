307a1dde8503712cc015851b2aeb580fd967a3d0
Ksenia <mineeva.94@inbox.ru>
2022-12-21 20:53:00 +0300

    Домашнее задание 5

---
+++ task36/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] num = new int[size];
+FillArrayRandomNum(num);
+Console.Write("Массив: ");
+PrintArray(num);
+int sum = 0;
+
+for (int x = 0; x < num.Length; x+=2)
+    sum = sum + num[x];
+
+    Console.WriteLine($"Cумма элементов cтоящих на нечётных позициях = {sum}");
+
+void FillArrayRandomNum(int[] num)
+{
+    for(int i = 0; i < num.Length; i++)
+        {
+            num[i] = new Random().Next(-100,100);
+        }
+}
+void PrintArray(int[] num)
+{
+    Console.Write(" ");
+    for(int i = 0; i < num.Length; i++)
+        {
+            Console.Write(num[i] + " ");
+        }
+    Console.Write(" ");
+    Console.WriteLine();
+}
+   
---
+++ task38/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76*/
+
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.Write("Массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int x = 0; x < numbers.Length; x++)
+{
+if (numbers[x] > max)
+    {
+    max = numbers[x];
+    }
+if (numbers[x] < min)
+    {
+    min = numbers[x];
+    }
+}
+
+Console.WriteLine($"Max = {max}, Min = {min}");
+Console.WriteLine($"Max - Min = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+for (int i = 0; i < numbers.Length; i++)
+    {
+    numbers[i] = Convert.ToDouble(new Random().Next(1, 1000)) / 100;
+    }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write(" ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write(" ");
+    Console.WriteLine();
+}
